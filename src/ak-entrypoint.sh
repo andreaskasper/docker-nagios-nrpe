94ab5dc3326f37f6abe442edc86cac96daa55998
Andreas Kasper <Andreas.Kasper@goo1.de>
2020-04-22 16:47:40 +0200

    Script for installing php-language

--- src/ak-entrypoint.sh
+++ src/ak-entrypoint.sh
@@ -6,6 +6,11 @@ echo "allowed_hosts=$ALLOWED_HOSTS" > /etc/nagios/nrpe_local.cfg
 
 #./nrpe-entrypoint.sh $@
 
+if [ "$INSTALL_PHP" = '1' ]; then
+    apt-get update
+    apt-get install php-cli
+fi
+
 if [ "$1" = 'nrpe' ]; then
     service nagios-nrpe-server start
     sleep infinity
